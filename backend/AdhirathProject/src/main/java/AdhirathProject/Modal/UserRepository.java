ff190375cffc26357ad0c5f08f33f0a41dc7864f
code-with-niteshkumar25 <niteshmca24@ipsacademy.org>
2025-05-05 17:47:23 +0530

    solve JPA annotation problem

---
+++ backend/AdhirathProject/src/main/java/AdhirathProject/Modal/UserRepository.java
@@ -0,0 +1,15 @@
+
+/*
+package AdhirathProject.Modal;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+Repository
+public class UserRepository extends JpaRepository<Users, Long>{
+    Optional<Users> findByUsername(String username);
+
+}
+*/
+
